8afccefc6a5d6c015f02c4abf49fc056dd940bf1
lshzang86 <lshzang86@naver.com>
2026-03-11 16:25:44 +0900

    Type Registry Error Log + NameSapce 추가

--- Runtime/TypeRegistry.cs
+++ Runtime/TypeRegistry.cs
@@ -2,28 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TypeRegistry<Tin, Tout>
+namespace BillotGames
 {
-    private Dictionary<Type, Tout> typeDict = new();
+    public class TypeRegistry<Tin, Tout>
+    {
+        private Dictionary<Type, Tout> typeDict = new();
 
+        public void Register<TType>(Tout target) where TType : Tin {
+            var targetType = typeof(TType);
+            if (!typeDict.ContainsKey(targetType)) {
+                Debug.Log($"new ({targetType}) registry registered");
+            }
 
-    public void Register<TType>(Tout target) where TType : Tin {
-        var targetType = typeof(TType);
-        if (!typeDict.ContainsKey(targetType)) {
-            Debug.Log($"new ({targetType}) registry registered");
+            typeDict[targetType] = target;
+        }
+        public void Unregister<TType>() where TType : Tin {
+            typeDict.Remove(typeof(TType));
         }
 
-        typeDict[targetType] = target;
-    }
-    public void Unregister<TType>() where TType : Tin {
-        typeDict.Remove(typeof(TType));
-    }
+        public bool TryGet(Tin targetType, out Tout result) {
+            if (typeDict.TryGetValue(targetType.GetType(), out result)) {
+                return true;
+            }
 
-    public bool TryGet(Tin targetType, out Tout result){
-        if (typeDict.TryGetValue(targetType.GetType(), out result)) {
-            return true;
+            Debug.LogError($"<color=red>type ({typeof(Tin)}) is not exist</color>");
+            return false;
         }
-
-        return false;
     }
 }
